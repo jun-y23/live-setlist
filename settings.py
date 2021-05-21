03d3fc187e9dfab8e113f887967d6a82aa7f23a4
jun-y23 <ki8109smith@gmail.com>
2021-05-21 20:25:52 +0900

    [fix] fix typo

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 import os
-from os,path import join,dirname
+from os.path import join,dirname
 from dotenv import load_dotenv
 
 load_dotenv(verbose=True)
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-SETLIST_API_KEY = os.environ.get(SETLIST_API_KEY)
+SETLIST_API_KEY = os.environ.get("SETLIST_API_KEY")

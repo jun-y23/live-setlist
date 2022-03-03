a3d02ed77e8e4ef52fc80684711bc1395f0f4732
jun-y23 <ki8109smith@gmail.com>
2022-03-03 17:06:26 +0100

    refactor

--- main.py
+++ main.py
@@ -1,58 +1,53 @@
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-import settings
-import urllib.request
-import os
+import ast
+import csv
 import json
 import math
-import ast
-import sys
-sys.setrecursionlimit(10000)
+import os
+import settings
+import urllib.request
 
-SETLIST_API_KEY = settings.SETLIST_API_KEY
-api_scope = ['https://www.googleapis.com/auth/spreadsheets',
-            'https://www.googleapis.com/auth/drive']
+setlist_api_key = settings.SETLIST_API_KEY
 
-# get Credentials
-credentials_path = os.path.join(os.path.expanduser('~'),'Downloads/sample-data-309705-d2a4f513fd15.json')
-credentials = ServiceAccountCredentials.from_json_keyfile_name(credentials_path, api_scope)
+# out csv
+# get id from musicbrainz
+url = 'https://api.setlist.fm/rest/1.0/artist/e6e879c0-3d56-4f12-b3c5-3ce459661a8e/setlists'
+# musicbrainz ID
+artist_mbid = '03ad1736-b7c9-412a-b442-82536d63a5c4'
+artist_name = 'elliottsmith'
 
-# get client
-gspread_client = gspread.authorize(credentials)
-spread_sheet_name = "jeff"
-
-sheet = gspread_client.open_by_key('1Ddrzu4LpVRaSwO8vECcbEjlXjL5fxEtQfxPG9gMLNnE')
-ss = sheet.worksheet('jeff')
-
-url = f'https://api.setlist.fm/rest/1.0/artist/e6e879c0-3d56-4f12-b3c5-3ce459661a8e/setlists'
 headers ={
         'Accept': 'application/json',
-        'X-API-Key': SETLIST_API_KEY
+        'X-API-Key': setlist_api_key
 }
 
-colnames = ['eventDate','url','venueName','cityName','CountryName']
 target_props = ['eventDate', 'url']
 target_list = []
 
 def main():
     fetch_all_setlist()
 
-    last_row =len(target_list)
-    ss.update('A1:B{}'.format(last_row), target_list)
+    write_csv(target_list)
 
 def fetch_all_setlist() -> None:
-    max_page_num =15
-    for page_num in range(1, 15):
-        url_with_params = f'https://api.setlist.fm/rest/1.0/artist/e6e879c0-3d56-4f12-b3c5-3ce459661a8e/setlists?p={page_num}'
+    url_with_params = 'https://api.setlist.fm/rest/1.0/artist/{}/setlists'.format(artist_mbid)
+    req = urllib.request.Request(url_with_params, headers=headers)
+
+    with urllib.request.urlopen(req) as res:
+        body = json.loads(res.read().decode('utf8'))
+        itemsPerPage = body['itemsPerPage']
+        total = body['total']
+        max_page_num = calcu_max_page_num(total, itemsPerPage)
+        dict_setlists = body['setlist']
+        for setlist in dict_setlists:
+            object_to_flat_array(setlist)
+
+    for page_num in range(2, max_page_num):
+        print(page_num)
+        url_with_params = 'https://api.setlist.fm/rest/1.0/artist/{}/setlists?p={}'.format(artist_mbid, page_num)
         req = urllib.request.Request(url_with_params, headers=headers)
 
         with urllib.request.urlopen(req) as res:
             body = json.loads(res.read().decode('utf8'))
-            print(page_num)
-            if page_num == 1:
-                itemsPerPage = body['itemsPerPage']
-                total = body['total']
-                max_page_num = calcu_max_page_num(total, itemsPerPage)
             dict_setlists = body['setlist']
             for setlist in dict_setlists:
                 object_to_flat_array(setlist)
@@ -66,8 +61,11 @@ def object_to_flat_array(target:dict) -> None:
         elif isinstance(value, dict):
             if key == 'venue':
                 item_array.append(value['name'])
-                item_array.append(value['city']['name'])
-                item_array.append(value['city']['country']['name'])
+                if 'city' in value:
+                    if 'name' in value['city']:
+                        item_array.append(value['city']['name'])
+                    if 'name' in value['city']['country']:
+                        item_array.append(value['city']['country']['name'])
         else:
             print(value)
     target_list.append(item_array)
@@ -75,4 +73,18 @@ def object_to_flat_array(target:dict) -> None:
 def calcu_max_page_num(total:int, itemPerPage:int) -> int:
     return math.ceil(total / itemPerPage)
 
-main()
+csv_columns = ['date', 'venue', 'city', 'country', 'uri']
+filename = 'setlists_{}.csv'.format(artist_name)
+
+def write_csv(list):
+    try:
+        with open(filename, 'w', newline='') as f:
+            w = csv.writer(f)
+            w.writerow(csv_columns)
+            w.writerows(list)
+        f.close()
+    except IOError:
+        print("I/O error")
+
+if __name__ == "__main__":
+    main()
